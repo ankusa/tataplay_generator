f2e45e9717871423a6ff93db5cc5d29f352d4e8d
ankush <webmaster.ankush@gmail.com>
2024-08-05 09:58:54 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,112 +1,139 @@
-import Head from 'next/head'; // Correct import statement for Head
+import Head from 'next/head';
 import { useEffect, useState } from 'react';
-import { Button, Form, Grid, Header, Message, Radio, Segment } from 'semantic-ui-react';
+import { Grid, Message, Segment, Icon, Image, Container, Header, Divider, Loader } from 'semantic-ui-react';
 
 export default function Home() {
-  const [dynamicUrl, setDynamicUrl] = useState("");
-  const [downloading, setDownloading] = useState(false);
+  const [shortUrl, setShortUrl] = useState("");
   const [err, setErr] = useState("");
+  const [visitorCount, setVisitorCount] = useState(0);
 
   useEffect(() => {
-  const url = window.location.origin.replace('localhost', '127.0.0.1') +
-    '/api/getM3u?sid=' + 'tplay' +
-    '_A&id=' + '123456789' +
-    '&sname=' + 'tataP' +
-    '&tkn=' + 'xeotpxyastrplg';
+    const url = `${window.location.origin.replace('localhost', '127.0.0.1')}/api/getM3u?sid=tplay_A&id=1028268934&sname=tataP&tkn=cheapgeeky.com`;
 
-  setDynamicUrl(url);
-}, []);
-
-  function downloadM3uFile(filename) {
-    setDownloading(true);
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-
-    fetch(window.location.origin + '/api/getM3u?sid=' + 'tplay' + '_' + 'A' + '&id=' + '123456789' + '&sname=' + 'tataP' + '&tkn=' + 'xeotpxyastrplg', requestOptions)
-      .then(response => response.text())
-      .then(result => {
-        console.log(result);
-        const data = result;
-        const blob = new Blob([data], { type: 'text/plain' });
-        if (window.navigator.msSaveOrOpenBlob) {
-          window.navigator.msSaveBlob(blob, filename);
-        }
-        else {
-          const elem = window.document.createElement('a');
-          elem.href = window.URL.createObjectURL(blob);
-          elem.download = filename;
-          document.body.appendChild(elem);
-          elem.click();
-          document.body.removeChild(elem);
-        }
-        setDownloading(false);
-      })
+    shortenUrl(url)
+      .then(short => setShortUrl(short))
       .catch(error => {
-        console.log('error', error);
-        setDownloading(false);
+        console.error('Error generating short URL:', error);
+        setErr('Error generating short URL. Please try refreshing the page.');
       });
+
+    fetchVisitorCount();
+  }, []);
+
+  async function shortenUrl(longUrl) {
+    try {
+      const response = await fetch('https://api-ssl.bitly.com/v4/shorten', {
+        method: 'POST',
+        headers: {
+          'Authorization': `Bearer ${process.env.NEXT_PUBLIC_BITLY_API_KEY}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ long_url: longUrl }),
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(`HTTP error! status: ${response.status}, message: ${errorData.message}`);
+      }
+
+      const data = await response.json();
+      return data.link;
+    } catch (error) {
+      throw new Error('Error shortening URL');
+    }
+  }
+
+  async function fetchVisitorCount() {
+    try {
+      const response = await fetch('/api/visitor-count');
+      if (!response.ok) {
+        const errorData = await response.json();
+        console.error('Error fetching visitor count:', errorData);
+        throw new Error(`HTTP error! status: ${response.status}, message: ${errorData.message}`);
+      }
+      const data = await response.json();
+      console.log('Visitor count data:', data);
+      setVisitorCount(data.count || 'No visitor count data');
+    } catch (error) {
+      console.error('Error:', error);
+      setVisitorCount('Error fetching visitor count');
+    }
   }
 
   return (
     <div>
       <Head>
-        <title>Generate Tata Play IPTV playlist</title>
-        <meta
-          name="description"
-          content="Easiest way to generate a Tata Play IPTV (m3u) playlist for the channels you have subscribed to."
-        />
+        <title>TATA PLAY COPY PASTE M3U</title>
+        <meta name="description" content="Easiest way to generate a Tata Play IPTV (m3u) playlist." />
       </Head>
-      <Grid columns='equal' padded centered>
-        <Grid.Row>
-          <Grid.Column></Grid.Column>
-          <Grid.Column computer={8} tablet={12} mobile={16}>
-            <Segment loading={downloading}>
-              <Header as={'h1'}>Generate Tata Play m3u</Header>
-              <Message>
-                <Message.Header>Dynamic URL to get m3u: </Message.Header>
-                <img src={`https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=${encodeURIComponent(dynamicUrl)}&size=small`} alt="QR Code" />
+
+      <Container text>
+        <Grid centered>
+          <Grid.Row>
+            <Grid.Column>
+              <Image src='https://upload.wikimedia.org/wikipedia/commons/2/29/Tata_Play_2022_logo.svg' size='huge' alt='TataPlay' centered />
+            </Grid.Column>
+          </Grid.Row>
+
+          <Grid.Row>
+            <Grid.Column computer={10} tablet={12} mobile={16}>
+              <Segment>
+                <Header as='h2'>
+                  <Icon name='linkify' />
+                  <Header.Content>M3U Short URL</Header.Content>
+                </Header>
+                {shortUrl ? (
+                  <Message positive>
+                    <p>
+                      <a href={shortUrl} target="_blank" rel="noreferrer">{shortUrl}</a> 🎉
+                    </p>
+                  </Message>
+                ) : (
+                  <Loader active inline='centered' size='large'>Generating short URL...</Loader>
+                )}
+                <Divider />
                 <p>
-                  <a href={dynamicUrl}>{dynamicUrl}</a>
+                  Use the M3U URL in the OTT Navigator or Tivimate app for all channels.
                 </p>
                 <p>
-                  You can use the above m3u URL in OTT Navigator or Tivimate app to watch all channels.
+                  Set data reload to 10 minutes and enjoy uninterrupted viewing!
                 </p>
-                <p>
-                  Set reload data to 10 Min in provider setting of Ott Navigator player &  Enjoy!
-                </p>
-                <Message.Header>You cannot generate a permanent m3u file URL on localhost but you can download your m3u file: </Message.Header>
-                <p></p>
-                <p>
-                  <Button loading={downloading} primary onClick={() => downloadM3uFile('ts.m3u')}>Download m3u file</Button>
-                </p>
-                <p>The downloaded m3u file will be valid only for 24 hours.</p>
-              </Message>
-            </Segment>
-          </Grid.Column>
-          <Grid.Column></Grid.Column>
-        </Grid.Row>
-        <Grid.Row style={{ display: err === '' ? 'none' : 'block' }}>
-          <Grid.Column></Grid.Column>
-          <Grid.Column computer={8} tablet={12} mobile={16}>
-            <Message color='red'>
-              <Message.Header>Error</Message.Header>
-              <p>
-                {err}
+                <p>The generated M3U URL is permanent and does not need to be refreshed every 24 hours. Enjoy!</p>
+                <p><strong>IMPORTANT:</strong> If you encounter an error with the generated URL, it might be due to an API issue. Simply wait for the API issue to be resolved.</p>
+              </Segment>
+              {err && (
+                <Message negative>
+                  <Message.Header><Icon name='exclamation circle' /> Error</Message.Header>
+                  <p>{err}</p>
+                </Message>
+              )}
+            </Grid.Column>
+          </Grid.Row>
+
+          <Grid.Row>
+            <Grid.Column>
+              <Divider />
+              <Header as='h3' textAlign='center'>
+                <Icon name='world' />
+                Visitor Information
+              </Header>
+              <p style={{ textAlign: 'center' }}>
+                Visitor Count: {visitorCount}
               </p>
-            </Message>
-          </Grid.Column>
-          <Grid.Column></Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column></Grid.Column>
-          <Grid.Column textAlign='center' computer={8} tablet={12} mobile={16}>
-            <a href="https://github.com/ExWhyZed9/tataplay_generator" target="_blank" rel="noreferrer">View source code on Github</a>
-          </Grid.Column>
-          <Grid.Column></Grid.Column>
-        </Grid.Row>
-      </Grid>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
+
+      <style jsx>{`
+        .message-container {
+          text-align: center;
+        }
+        .ui.image.huge {
+          max-width: 400px;
+          margin: auto;
+        }
+      `}</style>
     </div>
-  )
-}
+  );
+                  }
